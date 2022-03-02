d235cb16c85a8a7f59f25ab81756e22e8457a22c
MonikaJP <100354652+MonikaJP@users.noreply.github.com>
2022-03-02 16:15:40 +0530

    Update stepDefination.java
    added comment

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -18,7 +18,7 @@ public class stepDefination {
   @Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
     //code to navigate to login url
-    	System.out.println("navigated to login url");
+    	System.out.println("navigated to login url successfully");
     	
     }
   @Given("^validate the browser$")
